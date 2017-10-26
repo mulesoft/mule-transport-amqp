3b60bad80a277d89c9702136216e5e4ff03f5ed2
fsgonz <fabian.gonzalez@mulesoft.com>
2017-10-26 17:20:16 -0300

    AMQP-78: Fix DynamicRoutingKeyItCase so that the needed queues are created through a son file. (#135)

--- mule-transport-amqp/src/it/java/org/mule/transport/amqp/DynamicRoutingKeyItCase.java
+++ mule-transport-amqp/src/it/java/org/mule/transport/amqp/DynamicRoutingKeyItCase.java
@@ -14,13 +14,17 @@ import static org.mule.api.transport.PropertyScope.INVOCATION;
 import org.mule.api.MuleEvent;
 import org.mule.api.MuleMessage;
 import org.mule.transport.amqp.harness.AbstractItCase;
-
+import org.mule.transport.amqp.harness.rules.AmqpModelRule;
 import org.hamcrest.Matcher;
+import org.junit.ClassRule;
 import org.junit.Test;
 
 public class DynamicRoutingKeyItCase extends AbstractItCase
 {
 
+    @ClassRule
+    public static AmqpModelRule modelRule = new AmqpModelRule("dynamic-routing-key-config.json");
+    
     @Override
     protected String getConfigFile()
     {
