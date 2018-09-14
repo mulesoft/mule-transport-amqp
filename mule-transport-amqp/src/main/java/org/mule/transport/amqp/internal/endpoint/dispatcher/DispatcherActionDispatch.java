2722ef7d55bd1615cc029d81e8efa10fe2c411ed
fsgonz <fabian.gonzalez@mulesoft.com>
2018-09-14 14:45:18 -0300

    AMQP-99: Add RabbitMQ broker blocking handler in Mule 3 Connector. (#159)

--- mule-transport-amqp/src/main/java/org/mule/transport/amqp/internal/endpoint/dispatcher/DispatcherActionDispatch.java
+++ mule-transport-amqp/src/main/java/org/mule/transport/amqp/internal/endpoint/dispatcher/DispatcherActionDispatch.java
@@ -6,14 +6,19 @@
  */
 package org.mule.transport.amqp.internal.endpoint.dispatcher;
 
-import com.rabbitmq.client.Channel;
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.mule.transport.amqp.internal.connector.AmqpConnector;
 import org.mule.transport.amqp.internal.domain.AmqpMessage;
 
-import java.io.IOException;
+import com.rabbitmq.client.Channel;
 
 public class DispatcherActionDispatch extends DispatcherAction
 {
+    protected transient Log logger = LogFactory.getLog(getClass());
+
     public AmqpMessage run(final AmqpConnector amqpConnector,
                                 final Channel channel,
                                 final String exchange,
@@ -21,6 +26,11 @@ public class DispatcherActionDispatch extends DispatcherAction
                                 final AmqpMessage amqpMessage,
                                 final long timeout) throws IOException
     {
+        if (amqpConnector.isBlocked() && amqpConnector.isFailOnBlockedBroker())
+        {
+            logger.error("A blocking notification was received from the broker. Cannot publish message till unblocked." + amqpConnector.getBlockedReason());
+            throw new AmqpBlockedBrokerException(amqpConnector.getBlockedReason());
+        }
         channel.basicPublish(exchange, routingKey, amqpConnector.isMandatory(),
                 amqpConnector.isImmediate(), amqpMessage.getProperties(), amqpMessage.getBody());
 
