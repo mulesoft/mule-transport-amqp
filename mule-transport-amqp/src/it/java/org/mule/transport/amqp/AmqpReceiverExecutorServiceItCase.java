dff0c6ae853f4ade773b0d8f983bee4f2ab6964d
fabian.gonzalez <fabian.gonzalez@10.8.13.1>
2018-12-14 18:24:40 -0300

    AMQP-112: AMQP connector is overwritting threadpool.amqpReceiver from other instances of the connector.

---
+++ mule-transport-amqp/src/it/java/org/mule/transport/amqp/AmqpReceiverExecutorServiceItCase.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.transport.amqp;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.transport.amqp.harness.rules.AmqpModelRule;
+import org.mule.transport.amqp.internal.connector.AmqpConnector;
+
+public class AmqpReceiverExecutorServiceItCase extends FunctionalTestCase
+{
+	@ClassRule
+	public static AmqpModelRule modelRule = new AmqpModelRule("receiver-executor-service-config.json");
+	
+    @Override
+    protected String getConfigResources()
+    {
+        return "receiver-executor-service-config.xml";
+    }
+
+    @Test
+    public void testBothExecutorServiceNames() throws Exception
+    {
+		assertThat(((AmqpConnector) muleContext.getRegistry().lookupConnector("amqpConnector1")).getExecutorName(),
+				equalTo("amqpConnector1-amqpReceiver"));
+		assertThat(((AmqpConnector) muleContext.getRegistry().lookupConnector("amqpConnector2")).getExecutorName(),
+				equalTo("amqpConnector2-amqpReceiver"));
+    }
+}
